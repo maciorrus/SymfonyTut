ab28729ec5e15bdd2f70ff8a8591fe9ca2a2f404
Maciej Michalak <m.michalak@itkontrakt.com.pl>
2019-07-18 09:10:31 +0200

    Added simple profile

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -33,4 +33,12 @@ class SecurityController extends AbstractController
     {
         throw new \Exception('This method can be blank - it will be intercepted by the logout key on your firewall');
     }
+
+    /**
+     * @Route("/profile", name="app_userprofile")
+     */
+    public function profile()
+    {
+            return $this->render('security/profile.html.twig');
+    }
 }
